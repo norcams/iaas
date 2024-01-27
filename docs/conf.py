0812cf768633ff638c327b9101a705ed9d8fd431
Raymond Kristiansen <raymond.kristiansen@uib.no>
2024-01-27 10:35:33 +0100

    fix: try to fix css

--- docs/conf.py
+++ docs/conf.py
@@ -259,4 +259,4 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-html_style = 'css/theme.css'
+html_css_files = 'css/theme.css'
